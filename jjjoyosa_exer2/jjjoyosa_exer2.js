4f4b5f35a04b764437be3319b873f92a9a2a606d
jjjoyosa <jjjoyosa@up.edu.ph>
2024-03-01 11:31:48 +0800

    feat: added ValidatePassword function

---
+++ jjjoyosa_exer2/jjjoyosa_exer2.js
@@ -0,0 +1,36 @@
+function validatePassword(pass1, pass2) {
+  //checks if the passwords match
+  if (pass1 === pass2) {
+    //checks if characters are atleast 8
+    if (pass1.length >= 8 && pass2.length >= 8) {
+      //checks if there is atleast 1 digit
+      let count = 0;
+      for (let i = 0; i <= 9; i++) if (pass1.indexOf(i) !== -1) count = 1;
+      if (count === 0) return false;
+
+      //checks if there is atleast 1 uppercase
+      let ucount = 0;
+      let j = 0;
+      while (j <= pass1.length) {
+        var chara = pass1.charAt(j);
+
+        if (chara === chara.toUpperCase() && chara !== chara.toLowerCase())
+          ucount++;
+        j++;
+      }
+
+      //checks if there is atleast 1 lowercase
+      let lcount = 0;
+      let k = 0;
+      while (k <= pass1.length) {
+        var chara = pass1.charAt(k);
+
+        if (chara !== chara.toUpperCase() && chara === chara.toLowerCase())
+          lcount++;
+        k++;
+      }
+      if (lcount !== 0 && ucount !== 0) return true;
+      else return false;
+    } else return false;
+  } else return false;
+}
