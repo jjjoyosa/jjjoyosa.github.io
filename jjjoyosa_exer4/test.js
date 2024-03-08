1ad2c7676ab3e5e639970a195d83d00ef676bb98
jjjoyosa <jjjoyosa@up.edu.ph>
2024-03-08 11:37:46 +0800

    docs: Added documentations

--- jjjoyosa_exer4/test.js
+++ jjjoyosa_exer4/test.js
@@ -2,6 +2,6 @@
 import pkg from "./index.cjs";
 const { generateUniqueID, addAccount } = pkg;
 
-//calls the functions
+//calls the functions from index.js
 addAccount(["Tim", "Berners-Lee", "tim@w3c.com", 25]);
 addAccount(["Ted", "Nelson", "ted.n@w3c.com", 43]);
